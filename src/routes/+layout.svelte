0f7bfeee7b421fc4de8365b817ebe570d94e0db1
phil <p@pkpowell.net>
2022-12-13 13:53:16 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/assets">Assets</a>
+</nav>
+  
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Home</h1>
+
+<script lang="ts">
+    /** @type {import('./$types').PageData} */
+    export let data;
+  
+    console.log(data.sections); // [{ slug: 'profile', title: 'Profile' }, ...]
+</script>
