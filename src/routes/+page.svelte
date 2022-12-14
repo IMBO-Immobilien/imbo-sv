e88294d899902c2c8773f2f4a96b58138af44f33
phil <p@pkpowell.net>
2022-12-15 00:45:39 +0100

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div id="content" class="front-container">
     <h1 class="presentation">
         <div class="front-grid">
-            {data.site[0]}
+            <!-- {data} -->
             <!-- <Content class="welcome" :blocks="greet()" /> -->
             <!-- <Content class="company" :blocks="site.companyName" /> -->
         </div>
@@ -11,9 +11,17 @@
     </div>
 </div>
 
+<script context="module">
+    declare interface Data {
+        site: Site[]
+    }
+</script>
+
 <script lang="ts">
+    import type { Site } from '$lib/types'
+
     /** @type {import('./$types').PageData} */
-    export let data;
+    export let data:Data
     console.log("data",data.site[0])
     // import { site } from '../lib/store'
   
