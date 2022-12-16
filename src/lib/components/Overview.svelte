d24df811e3c8757cc2089d04b3929b763bdee91f
phil <p@pkpowell.net>
2022-12-16 13:44:33 +0100

    updates

--- src/lib/components/Overview.svelte
+++ src/lib/components/Overview.svelte
@@ -1,12 +1,51 @@
-{#each assets as a}
-<div>{ a.nameDE }</div>
+<div>
+    <!-- <h2>Unsere Angebote</h2> -->
+    {#each assets as a}
+    <a 
+        class="asset-overview" 
+        href="{'/assets-asset' + a.slug.current}"
+    >
+        <div class="asset front-grid">
+            <!-- {{a.overviewImage.dimensions}} -->
+            <img-src
+                :width="300"
+                :src="a.overviewImage.url"
+                :quality="50"
+                :crop="a.overviewImage.crop"
+                :dimensions="a.overviewImage.dimensions"
+                fit="inside"
+                :loading="true"
+                class="thumb"
+            />
+                <!-- :imgAsset="a.overviewImage" -->
+            <div class="asset-text">
+                <div class="asset-name">{{getText(a, 'name')}}</div>
+                <Content class="asset-description" blocks="{getText(a, 'generalShort')}" />
+            </div>
+        </div>
+    </n-link>
+</div>
+
 {/each}
 
 <script lang="ts">
-    // import { get } from 'svelte/store'
-    // import { siteData } from '$lib/store'
     import type { Asset } from '$lib/types'
     // const site = get(siteData)
+    const getText = (a: Asset, k: string):string => {
+            let keyDE = k + "DE"
+            let keyEN = k + "EN"
+            switch (this.$i18n.locale) {
+                case "de":
+                    return a[keyDE]
+
+                default:
+                    if (typeof a[keyEN] !== "undefined"){
+                        return a[keyEN]
+                    } else {
+                        return a[keyDE]
+                    }
+            }
+        }
 
     export let assets:Asset[] = []
 </script>
---
+++ src/lib/components/imgSrc.svelte
@@ -0,0 +1,23 @@
+<div class="img-container">
+    <img
+        src={getURL()}
+        style={
+            ...opacity, 
+            aspectRatio: aspectRatio
+        }
+        width="100%"
+        height="auto"
+        alt="alt"
+        @load="loaded"
+        class="fitinside fadein img"
+        loading="lazy"
+    >
+</div>
+
+<script lang="ts">
+import { Dimensions, type People, type Crop, type Hotspot, type IMG } from '$lib/types'
+let dimentions = Dimensions{}
+let copyright = People{}
+let crop = Crop{}
+
+</script>
