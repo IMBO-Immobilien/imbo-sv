c5025e6f63b096ce49611dde46fdb5a1addda34e
phil <p@pkpowell.net>
2022-12-16 17:04:04 +0100

    updates

--- src/lib/components/imgSrc.svelte
+++ src/lib/components/imgSrc.svelte
@@ -15,9 +15,20 @@
 </div>
 
 <script lang="ts">
+declare interface Style {
+    [tag: string]: string
+}
 import { Dimensions, type People, type Crop, type Hotspot, type IMG } from '$lib/types'
-let dimentions = Dimensions{}
-let copyright = People{}
-let crop = Crop{}
+export let dimentions = Dimensions{}
+export let copyright = People{}
+export let crop = Crop{}
+export let hotspot = Hotspot{}
+export let imgAsset = IMG{}
+export let opacity = Style{}
+export let height = 500
+export let width = 1200
+export let src = ""
+export let alt = ""
+export let lazy = "lazy"
 
 </script>
